732cd656b46d28ca0f444ec99d64f3556d86c895
Salvatore Emilio <razielsasy@gmail.com>
2021-08-12 12:16:15 +0200

    now hel read errors aswell

--- internal/models/graphql.go
+++ internal/models/graphql.go
@@ -1,7 +1,12 @@
 package models
 
-type Request struct {
+type GraphQLRequest struct {
 	Query         string                  `json:"query"`
 	OperationName *string                 `json:"operationName,omitempty"`
 	Variables     *map[string]interface{} `json:"variables,omitempty"`
+}
+
+type GraphQLResponse struct {
+	Data   *interface{}   `json:"data,omitempty"`
+	Errors *[]interface{} `json:"errors,omitempty"`
 }
--- internal/models/response.go
+++
@@ -1,6 +0,0 @@
-package models
-
-type Response struct {
-	Data   *interface{}   `json:"data,omitempty"`
-	Errors *[]interface{} `json:"errors,omitempty"`
-}
