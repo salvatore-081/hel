b11c19c8e486bfa11e1316d36894832691d2fa1c
Salvatore Emilio <razielsasy@gmail.com>
2021-08-28 19:21:19 +0200

    implemented graphql error policy

--- hel.go
+++ hel.go
@@ -1,53 +1,77 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"errors"
 	"fmt"
+	"net/http"
 
-	"github.com/salvatore-081/hel/pkg"
+	"github.com/salvatore-081/hel/models"
 )
 
-func NewClient(host string) pkg.Client {
-	return pkg.Client{
-		Url: host,
+type Client struct {
+	Url  string
+	Opts models.Opts
+}
+
+func (c *Client) Do(query string, operationName *string, variables *map[string]interface{}, v interface{}) error {
+	gqlRequest := models.GraphQLRequest{
+		Query:         query,
+		OperationName: operationName,
+		Variables:     variables,
 	}
-}
 
-func main() {
-	// client := NewClient("https://api.spacex.land/graphql/")
-	// 	query := `
-	// query{
-	//   ships{
-	//     name
-	//   }
-	// }
-	//   `
-	// 	type Ships struct {
-	// 		Ships []struct {
-	// 			Name string
-	// 		}
-	// 	}
-	// 	var ships interface{}
-	var user map[string]interface{}
-	var errors []map[string]interface{}
-	client := NewClient("http://localhost:8081/graphql")
-	query := `
-query{
-  user(username:"diagnostc"){
-    id
-    organization{
-      id
-      name
-      eid
-    }
-  }
-}
-`
-
-	e := client.Do(query, nil, nil, &user)
+	jsonEncodedRequest, e := json.Marshal(gqlRequest)
 	if e != nil {
-		fmt.Println(fmt.Sprintf("DO ERROR => %s", e.Error()))
+		return e
 	}
-	fmt.Println(fmt.Sprintf("data => %v", user))
-	fmt.Println(fmt.Sprintf("errors => %v", errors))
 
+	buffer := bytes.NewBuffer(jsonEncodedRequest)
+
+	response, e := http.Post(c.Url, "application/json", buffer)
+	if e != nil {
+		return e
+	}
+	defer response.Body.Close()
+
+	var gqlResponse models.GraphQLResponse
+
+	e = json.NewDecoder(response.Body).Decode(&gqlResponse)
+	if e != nil {
+		return e
+	}
+
+	if gqlResponse.Data != nil && (c.Opts.ErrorPolicy != 0 || gqlResponse.Errors == nil) {
+		data, e := json.Marshal(gqlResponse.Data)
+		if e != nil {
+			return e
+		}
+
+		decoder := json.NewDecoder(bytes.NewReader(data))
+		decoder.UseNumber()
+		decoder.DisallowUnknownFields()
+
+		e = decoder.Decode(&v)
+		if e != nil {
+			return e
+		}
+	}
+
+	if gqlResponse.Errors != nil && c.Opts.ErrorPolicy != 1 {
+		gqlErrors, e := json.MarshalIndent(gqlResponse.Errors, "", " ")
+		if e != nil {
+			return e
+		}
+		return errors.New(fmt.Sprintf("%s", string(gqlErrors)))
+	}
+
+	return nil
+}
+
+func NewClient(host string, opts models.Opts) Client {
+	return Client{
+		Url:  host,
+		Opts: opts,
+	}
 }
